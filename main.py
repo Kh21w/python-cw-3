c21ada10e9ae274dc9aa6b451f59f0a9dd5b103f
Kh21w <Khaldaaan@gmail.com>
2023-09-04 01:53:26 +0300

    تم حل التمرين

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-# write your code here
+
+
+#Part 1
+
+
+Favaourite_Animals = ['Dolphin', 'Python', 'Chameleon', 'Hoopoe']
+print (Favaourite_Animals)
+print (Favaourite_Animals[1])
+Favaourite_Animals.remove('Chameleon')
+
+
+#Part 2
+
+
+Favaourite_Animals.append('Penguin')
+for Animal in Favaourite_Animals:
+    print (f"I love {Animal}")
+
+
+#Part 3
+
+
+Numbers = [5, 4, 3, 2, 1]
+Number_Sum = 0
+for Number in Numbers:
+    Number_Sum += Number
+print (Number_Sum)
